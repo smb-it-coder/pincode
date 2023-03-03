7b67f0c905b8e81806e65377175c05f12a4f33ab
smb-it-coder <brijsingh080@gmail.com>
2023-03-03 08:28:20 +0530

    added home page seo content

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -2,7 +2,7 @@ import React, { useState, useReducer, Fragment } from 'react';
 import SelectState from '../util/SelectState';
 import { useNavigate } from "react-router-dom";
 import Alert from 'react-bootstrap/Alert';
-
+import { Helmet } from "react-helmet";
 
 const formReducer = (state, event) => {
     return {
@@ -29,16 +29,21 @@ function BodyForHome() {
 
     return (
         <Fragment>
+             <Helmet>
+                <title>Location PIN Code | India Pincode Number | Search My Pincode </title>
+                <meta name="description" content={`Find PIN code and post office details for any location, area, village, city, district or state in India at SearchMyPincode.`} />
+                <meta name="keywords" content={`my location pincode, area pincode, city pin code, pin code search, my pin code, pin code India, pin code of my current location, India postal index number, 6 digit pin code`}  />
+                <link href="/" rel="canonical" />
+                <meta http-equiv="Content-Language" content="English" />           
+            </Helmet>
             <div className="container-fluid bg-grey">
                 <div className="row">
                     <div className="col-sm-2">
                         <span className="glyphicon glyphicon-globe logo slideanim"></span>
                     </div>
                     <div className="col-sm-8">
-                        <h1>INDIA PIN CODES SEARCH</h1>
-                        
-                            <div className="row"><p>Postal code of any location in India is just a single click away!
-                            Give it a try by yourself!</p></div>
+                    <h1>Search Pin Code by Location</h1>
+                    <div className="row"><p>Postal code of any location in India is just a single click away! Give it a try by yourself!</p></div>
                         <div className="row">&nbsp; </div>
                         <form className="form-inline" onSubmit={handleSubmit} >
                             <div className="input-group">
@@ -61,7 +66,7 @@ function BodyForHome() {
 
                                 {['warning'].map((variant) => (
                                     <Alert key={variant} variant={variant}>
-                                        Please  Select Your State
+                                        <h2>State Wise Select Your State For Pin code  :</h2>
                                     </Alert>
                                 ))}
                             </h3>
@@ -72,12 +77,17 @@ function BodyForHome() {
                         </div>
 
                         <div className="row">&nbsp; </div>
+
+                        
+                       
+
                     </div>
 
                     <div className="col-sm-2">
                         <span className="glyphicon glyphicon-globe logo slideanim"></span>
                     </div>
                 </div>
+                
             </div>
         </Fragment>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -35,11 +35,36 @@ const Home = () => {
         <div className="row">
           <div className="col-sm-2"><span className="glyphicon glyphicon-globe logo slideanim"></span> </div>
           <div className="col-sm-8">
-          <h3>First Two Digit of the Postal Codes belongs to:</h3>
-            <TwoDigitPincodeBelongsToState /> 
-            </div>
+            <h3>First Two Digit of the Postal Codes belongs to:</h3>
+            <TwoDigitPincodeBelongsToState />
+            <div className="row"> &nbsp;&nbsp;</div>
+            <div className="row"> 
+
+                          <h2>India Pin code FAQs</h2>
+                           <h3>What is pin code?</h3>
+                           <p>PIN code means Postal Index Number which is a 6 digit code used by Indian Government in postal code system.</p>
+
+                           <h3>Q: What is the pin code of my location?</h3>
+                           <p>Ans: You can find your location pin code here. Search your state or district and then select your location. 
+                            You will find your location pin code</p>
+
+                            <h3>Q: How many post offices are there in India</h3>
+                           <p>Ans: There are 1,56,434 post offices functioning in India including rural areas. </p>
+                           <h3>Q: What is the meaning of 6 digit in postal code?</h3>
+                           <p>Ans: India pin code has 6 digits. First digit ranges from 1 to 9 and represents for postal zone. 
+                            Second digit represents for Sub regions and 3 digit is for district. The combination of third and fourth digits are for state’s 
+                            location sorting district. The 5th and 6th digit denote a post office.</p>
+
+                           <h3>Q: When did PIN code system started in India?</h3>
+                           <p>Ans: PIN code system was first introduced in India on August 15, 1972 by Shriram Bhikaji Velankar.</p>
+
+
+
+                         </div>
+          </div>
           <div className="col-sm-2"> <span className="glyphicon glyphicon-globe logo slideanim"></span> </div>
         </div>
+       
       </div>
       <br/>
       <Footer />
